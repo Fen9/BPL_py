20b64ccca2901fcbdae18fda8421ff58eaf1f23e
YuxingQiu <yuxqiu@gmail.com>
2020-06-16 00:33:29 -0700

    add sampler

---
+++ sampler.py
@@ -0,0 +1,8 @@
+class CPD:
+    def __init__():
+        pass
+    
+    @staticmethod
+    def sample_relation_token(libclass, eval_spot_type):
+        eval_spot_token = eval_spot_type + libclass.tokenvar.sigma_attach.*randn;
+
