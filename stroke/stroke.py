c2d5f5f52387ecca1bb7468c3c5e7b271bcf6dab
Feng <fenggao.usa@gmail.com>
2020-06-16 19:43:54 -0700

    update stroke

--- stroke/stroke.py
+++ stroke/stroke.py
@@ -30,33 +30,79 @@ class stroke():
         self._shapes_token = copy.deepcopy(oldS._shapes_token)
 
     def get_ids(self):
-        return self._ids
+        if self._ids == None:
+            self._ids = copy.deepcopy(self._my_type._ids)
+        out = self._ids
+        return out
     
     def get_invscales_type(self):
-        return self._invscales_type
+        if self._invscales_type == None:
+            self._invscales_type = copy.deepcopy(self._my_type.invscales_type)
+        out = self._invscales_type
+        return out
 
     def get_shapes_type(self):
-        return self._shapes_type
+        if self._shapes_type == None:
+            self._shapes_type = copy.deepcopy(self._my_type._shapes_type)
+        out = self._shapes_type
+        return out
 
     def get_R(self):
-        return self._R
+        if self._R == None:
+            self._R = copy.deepcopy(self._my_type._R)
+        out = self._R
+        if len(self._R) == 0 and self._my_type._R._type == 'mid':
+            out._eval_spot_token = copy.deepcopy(self._eval_spot_token)
+        return out
 
     def set_ids(self, ids):
-        self._ids = ids
+        self._my_type._ids = ids
     
     def set_invscales_type(self, invscales_type):
-        self._invscales_type = invscales_type
+        self._my_type._invscales_type = invscales_type
 
     def set_shapes_type(self, shapes_type):
-        self._shapes_type = shapes_type
+        self._my_type._shapes_type = shapes_type
 
     def set_R(self, R):
-        self._R = R
+        self._my_type._R = R
+        if len(self._my_type._R) == 0 and self._my_type._R._type == 'mid':
+            self._my_type._R._eval_spot_token = []
+            self._eval_spot_token = R._eval_spot_token
 
     def get_num_substrokes(self):
         return len(self._ids)
 
+    def get_motor(self):
+        motor = None
+        if self._cache_current:
+            motor = self._cache_motor
+        else:
+            motor, motor_spline = vanilla_to_motor(self._shapes_token, self._invscales_token, self._pos_token)
+            self._cache_motor = motor
+            self._cache_motor_spline = motor_spline
+            self._cache_current = True
+        return motor
+
     def get_motor_spline(self):
+        motor_spline = None
+        if self._cache_current:
+            motor_spline = self._cache_motor_spline
+        else:
+            motor, motor_spline = vanilla_to_motor(self._shapes_token, self._invscales_token, self._pos_token)
+            self._cache_motor = motor
+            self._cache_motor_spline = motor_spline
+            self._cache_current = True
+        return motor
+
+    def get_eval_spot_token(self):
+        out = self._eval_spot_token
+        return out
+
+    def on_listener(self):
+        pass
+
+    def save_obj(self):
         pass
 
     @staticmethod
@@ -66,3 +112,16 @@ class stroke():
     @staticmethod
     def needs_update(_, event):
         event.AffectedObject.cache_current = False
+
+
+def vanilla_to_motor(vanilla_shapes, invscales, firest_ops):
+    if np.all(vanilla_shapes==0) or np.all(invscales == 0) or np.all(first_ops==0):
+        motor = []
+        motor_spline = []
+        return motor, motor_spline
+
+        ncpt, _, n = vanilla_shapes.shape
+        for i in range(0, n):
+            vanilla_shapes[:,:,i] = invscales[i] * vanilla_shapes[:,:i]
+
+        vanilla_traj = np.array((n, 1))
